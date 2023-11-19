4109229860d7075d5e353a21e86b2693d85a4184
jerpol <jerpol01@gmail.com>
2023-11-19 12:14:24 -0500

    initial commit

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+const botonMostrar = document.querySelectorAll('.show-modal');
+const vModal = document.querySelector('.modal');
+const cortina = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+const abreModal = function(){
+    vModal.classList.remove('hidden');
+    cortina.classList.remove('hidden');
+}
+
+const cierraModal = function(){
+    vModal.classList.add('hidden');
+    cortina.classList.add('hidden');
+}
+
+for (let i=0; i<botonMostrar.length;i++){
+    botonMostrar[i].addEventListener('click',abreModal);
+};
+closeModal.addEventListener('click',cierraModal);
+cortina.addEventListener('click',cierraModal);
