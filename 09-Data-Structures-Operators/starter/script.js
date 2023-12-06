2142c0553cf3604d952204f38bf57dd72eaf8126
jerpol <jerpol01@gmail.com>
2023-12-06 16:09:48 -0500

    initial commit

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,118 @@ const restaurant = {
     },
   },
 };
+
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+      [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+      ],
+      [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+      ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
+
+  //1)
+
+  const [players1, players2] = game.players;
+  //onst players1 = game.players[0]; 
+  //const players2 = game.players[1];
+  console.log(players1, players2);
+
+  //2)
+
+  const [gk, ...fieldPlayers] = players1;
+  console.log(gk, fieldPlayers);
+
+  //3)
+  
+  const allPlayers = [...players1, ...players2];
+  console.log(allPlayers);
+
+  //4)
+  const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+  console.log (players1Final);
+
+  //5)
+const {team1, x:draw, team2} = {...game.odds};
+console.log(team1, draw, team2);
+
+//6)
+function printGoals(...p){
+  console.log(...p, p.length);
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+//7)
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+
+//1)
+for (const [i,jug] of game.scored.entries()){
+  console.log(`El Gol ${i+1} fue hecho por ${jug}`);
+};
+
+//2)
+const odds = Object.values(game.odds);
+console.log(odds);
+let media = 0;
+for (const odd of odds) media += odd;
+media /= odds.length;
+console.log(media);
+
+//3)
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  console.log(team, odd, game[team]);
+}
+
+//4)
+let goleador = {};
+for (const jugador of game.scored) {
+  console.log(jugador);
+  goleador[jugador] ? goleador[jugador]++ : (goleador[jugador] = 1) ; 
+}
+console.log(goleador);
+const miobjeto = {};
+for (let i=1; i<10; i++){
+miobjeto[`pep`+i] = i+1;
+}
+console.log(miobjeto);
